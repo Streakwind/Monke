f5a28a94012ca928ee1a1bbc8ff7cde9870a5d44
Streakwind <79235229+Streakwind@users.noreply.github.com>
2021-02-19 20:18:56 -0800

    Update Main.py

--- cogs/Main.py
+++ cogs/Main.py
@@ -1,6 +1,24 @@
 from discord.ext import commands
 import discord
 
+#class HelpCommand(commands.HelpCommand):
+    #async def send_bot_help(self, mapping):
+        # here is where you send the main help command message.
+        # you can get all the commands with `bot.commands()`.
+        # self.bot exists here, and self.context too if you need it (which is ctx)
+
+    #async def send_cog_help(self, cog):
+        # this is where you send the cog help command message.
+        # you can get all of a cog's commands with `cog.commands`
+
+    #async def send_group_help(self, group):
+        # this is where you send the group help command message.
+        # you can get all of a group's subcommands with `group.commands`
+
+    #async def send_command_help(self, group):
+        # this is where you send the command help command message.
+        # this is similar to send_group_help, but you don't need to worry about subcommands
+        
 class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -19,6 +37,10 @@ class Main(commands.Cog):
        owner = self.bot.get_user(self.bot.owner_id)
        await owner.send(f"{ctx.author} has sent you a message!\n{arg}")
        await ctx.send(f"Succesfully sent {arg}")
+    @commands.command()
+    async def sourcecode(self, ctx)
+        """Don't....."""
+        await ctx.send(f"`https://github.com/Streakwind/Monke`")
 # Setting `Playing ` status
 #await bot.change_presence(activity=discord.Game(name="a game"))
 
