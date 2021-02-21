00e8b289946699c39996aa6d346b6014b016e500
Streakwind <79235229+Streakwind@users.noreply.github.com>
2021-02-20 16:06:53 -0800

    Update Main.py

--- cogs/Main.py
+++ cogs/Main.py
@@ -28,20 +28,7 @@ class Main(commands.Cog):
     async def secret(self, ctx):
         """I wonder what this does...."""
         await ctx.send("```Never gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you```")
-    @commands.has_permissions(ban_members=True)
-    @commands.bot_has_permissions(ban_members=True)
-    @commands.command()
-    async def ban(self, ctx, member: discord.Member, *, banreason=None):
-      """Bans a specified user from the guild"""
-      await member.ban(reason=banreason)
-      await ctx.send(f"{member} has been banned by {ctx.author}. Reason: {banreason}")
-    @commands.has_permissions(kick_members=True)
-    @commands.bot_has_permissions(kick_members=True)
-    @commands.command()
-    async def kick(self, ctx, member: discord.Member, *, kickreason=None):
-      """Kicks a specified user from the guild"""
-      await member.kick(reason=kickreason)
-      await ctx.send(f"{member} has been kicked by {ctx.author}. Reason: {kickreason}")
+
                
 def setup(bot):
     bot.add_cog(Main(bot))
